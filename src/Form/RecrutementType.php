8453ba7d35945f1ef296216a8e66edee6a9f87e5
Guellier Paul <guellier.paul@gmail.com>
2023-12-08 17:01:22 +0100

    upload

--- src/Form/RecrutementType.php
+++ src/Form/RecrutementType.php
@@ -6,13 +6,38 @@ use App\Entity\Recrutement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class RecrutementType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('brochure', FileType::class, [
+        ->add('cv', FileType::class, [
+            'label' => 'Brochure (PDF file)',
+
+            // unmapped means that this field is not associated to any entity property
+            'mapped' => false,
+
+            // make it optional so you don't have to re-upload the PDF file
+            // every time you edit the Product details
+            'required' => false,
+
+            // unmapped fields can't define their validation using attributes
+            // in the associated entity, so you can use the PHP constraint classes
+            'constraints' => [
+                new File([
+                    'maxSize' => '1024k',
+                    'mimeTypes' => [
+                        'application/pdf',
+                        'application/x-pdf',
+                    ],
+                    'mimeTypesMessage' => 'Please upload a valid PDF document',
+                ])
+            ],
+        ])
+        ->add('lettremotiv', FileType::class, [
             'label' => 'Brochure (PDF file)',
 
             // unmapped means that this field is not associated to any entity property
