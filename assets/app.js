8e09defafa925f6d0fb247294c300ffaec719499
florentin777 <florentinsibert7@gmail.com>
2023-12-08 17:25:05 +0100

    js=css
    css= js

--- assets/app.js
+++ assets/app.js
@@ -6,4 +6,22 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+
 import './styles/app.css';
+
+const slide = ["{{ asset('assets/images/bouteille1.png')}}",
+"{{ asset('assets/images/bouteille2.png')}}",
+"{{ asset('assets/images/bouteille3.png')}}",
+"{{ asset('assets/images/bouteille4.png')}}",
+"{{ asset('assets/images/bouteille5.png')}}",
+"{{ asset('assets/images/bouteille6.png')}}"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
